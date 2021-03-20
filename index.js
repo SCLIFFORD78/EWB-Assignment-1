d92ed6c9482674753981636863c5e831d3bed656
Sheamus Clifford <59657367+SCLIFFORD78@users.noreply.github.com>
2021-03-20 23:24:57 +0000

    Update index.js
    Additions for glitch deployment

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ const Joi = require("@hapi/joi");
 env.config();
 
 const server = Hapi.server({
-  port: 3000,
-  host: "localhost",
+  port: process.env.PORT || 3000,
 });
 
 async function init() {
