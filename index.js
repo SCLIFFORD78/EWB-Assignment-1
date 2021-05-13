51154310080a3c9c28de50226571a97402641f7d
SCLIFFORD78 <faugh01@gmail.com>
2021-05-13 01:26:04 +0100

    no message

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
 "use strict";
 
-
-
 const Hapi = require("@hapi/hapi");
 const Inert = require("@hapi/inert");
 const Vision = require("@hapi/vision");
 const Handlebars = require("handlebars");
 const Cookie = require("@hapi/cookie");
-require('./app/models/db');
-const env = require('dotenv');
 const Joi = require("@hapi/joi");
+const utils = require("./app/api/utils.js");
+require("./app/models/db");
+const env = require("dotenv");
 
+const dotenv = require("dotenv");
 
-
-
-env.config();
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
 
 const server = Hapi.server({
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 4000,
+  routes: { cors: true },
 });
 
 async function init() {
   await server.register(Inert);
   await server.register(Vision);
   await server.register(Cookie);
+  await server.register(require("hapi-auth-jwt2"));
+  server.validator(require("@hapi/joi"));
   server.views({
     engines: {
       hbs: require("handlebars"),
@@ -39,12 +44,19 @@ async function init() {
     cookie: {
       name: process.env.cookie_name,
       password: process.env.cookie_password,
-      isSecure: false
+      isSecure: false,
     },
     redirectTo: "/",
   });
+  server.auth.strategy("jwt", "jwt", {
+    key: "secretpasswordnotrevealedtoanyone",
+    validate: utils.validate,
+    verifyOptions: { algorithms: ["HS256"] },
+  });
+
   server.auth.default("session");
   server.route(require("./routes"));
+  server.route(require("./routes-api"));
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
 }
@@ -53,7 +65,5 @@ process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
-
-server.validator(require("@hapi/joi"));
 
 init();
